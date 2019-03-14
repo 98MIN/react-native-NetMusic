6122b5f25f435b561399e7aaf790f7316de65d36
98MIN <1033312504@qq.com>
2019-03-14 16:17:08 +0800

    增加搜索界面

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
-import { SearchBar } from 'react-native-elements'
-import { View ,Text } from 'react-native'
+import { SearchBar, ListItem } from 'react-native-elements'
+import { View ,Text, FlatList } from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 import setAxios from '../../utils/axios'
 import { debounce } from '../../utils/utils'
 
@@ -18,21 +19,59 @@ class Search extends React.Component {
     debounce(this.handleSearchInfo(search))
   };
   handleSearchInfo = search => {
-    setAxios(`search?keywords=${search}`).then(v=>{
-      adapterSearchResult(v.result.songs)
-    })
+    setAxios(`search?keywords=${search}`).then(v=>
+       this.setState({
+          search_result: adapterSearchResult(v.result.songs)
+      })
+    )
   }
+  handleList = (musicId, musicName, authorNames, musicTime, picUrl, e) => {
+    this.props.navigation.navigate('Play', { musicId, musicName, authorNames, musicTime, picUrl })
+  }
+  keyExtractor = (({item,index}) => index.toString())
+  renderItem = (({item,index}) => {
+    return (
+        <ListItem
+        title={ item.song_name }
+        subtitle={ item.singer_name }
+        leftAvatar={{source: { uri: item.singer_pic }}}
+        onPress={this.handleList.bind(
+          this,
+          item.song_id,
+          item.song_name,
+          item.singer_name,
+          item.song_time,
+          item.singer_pic
+        )}
+        titleStyle={{ fontSize: 13, color: 'rgba(0,0,0,1)' }}
+        subtitleStyle={{ fontSize: 12, marginTop: 5 }}
+        />
+    )
+  })
   render() {
     const { search } = this.state
     return (
       <View>
         <SearchBar
-          placeholder="Type Here..."
+          placeholder="请输入歌曲名..."
           onChangeText={this.updateSearch}
           value={search}
           platform='default'
+          placeholderTextColor='#fff'
+          containerStyle={{ backgroundColor: 'rgb(206,19,33)' }}
+          inputContainerStyle={{ backgroundColor: 'rgba(85, 85, 85, 0.3)'}}
           round={true}
+          inputStyle={{ fontSize: 15, color: '#fff' }}
+          leftIconContainerStyle={{color:'#fff'}}
+          searchIcon={<Icon name='search' color={'#fff'} size={16}/>}
+          clearIcon={<Icon name='x' color={'#fff'} size={16}/>}
         />
+        <FlatList
+          data={this.state.search_result}
+          key={this.keyExtractor}
+          renderItem={this.renderItem}
+        />
+        <View style={{marginBottom: 50}}></View>
       </View>
     );
   }
@@ -43,11 +82,13 @@ export default Search;
 function adapterSearchResult(data){
   let result = []
   result = data.map(v=>{
+    console.log(v)
     return {
       singer_pic : v.artists.map(vv=>vv.img1v1Url).join(''),
-      singer_name : v.artists.map(vv=>vv.name).join(''),
-      song_name : v.album.name,
-      song_id : v.album.id
+      singer_name : v.artists.map(vv=>vv.name).join('-'),
+      song_name : v.name,
+      song_id : v.id,
+      song_time : v.duration
     }
   })
 
